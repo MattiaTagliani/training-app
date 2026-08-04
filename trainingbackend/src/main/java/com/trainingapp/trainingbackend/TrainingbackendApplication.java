48223cac2ee32841646c44088f221c3f9acbb041
mattia.tagliani <mattia.tagliani@gmail.com>
2026-08-04 13:43:33 +0200

    initial folder structure

---
+++ trainingbackend/src/main/java/com/trainingapp/trainingbackend/TrainingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.trainingapp.trainingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingbackendApplication.class, args);
+	}
+
+}
---
+++ trainingbackend/src/test/java/com/trainingapp/trainingbackend/TrainingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingapp.trainingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
